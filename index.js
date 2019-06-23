fea071b93120ae34cd7b1a4b74834aedcbe3ffbd
Your Name <you@example.com>
2019-06-23 17:54:59 -0300

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+io.set('origins', '*github*:*');
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+
+
+io.on('connection', function(socket){
+
+  socket.on('user move', function(msg){
+    io.emit('user move', msg);
+  });
+});
+var port = process.env.PORT || 3000
+http.listen(port, function(){
+  console.log('listening on *:80');
+});
